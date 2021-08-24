437d02354f7a245ebb3bee376f973efe8e606ad1
isademir.personal@outlook.com <isademir.personal@outlook.com>
2021-08-24 13:35:26 +0300

    Add project files.

---
+++ ConstructorMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConstructorMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "İsa", LastName = "Demir", City = "Ordu" };
+
+            Customer customer2 = new Customer(2, "Züleyha", "Demir", "Ordu");
+
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+
+    class Customer
+    {
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        //ctor -> yazıp tab tab yaparsak otomatik yazar
+        public Customer() //Default Constructor
+        {
+            Console.WriteLine("Constructor Çalıştı");
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("Bursa");
+
+            Console.WriteLine(sehirler.Count);
+            Console.WriteLine(sehirler2.Count);
+
+            foreach (var item in sehirler2.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
